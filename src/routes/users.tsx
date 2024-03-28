542769f2a3f2de3c82150000baecdbe737451455
Jdallos <joshdallos@gmail.com>
2024-03-28 12:18:09 +0200

    Clean user data after creating user & add column Docs

--- src/routes/users.tsx
+++ src/routes/users.tsx
@@ -56,6 +56,9 @@ const UsersIndexRoute = () => {
     setLoading(false);
   };
 
+  /**
+   * Grid column definitions for users table
+   */
   const columns: GridColDef[] = [
     {
       field: "firstName",
