94d218e0d99ad1786e056e625fe7ac72a5a3fd3d
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2024-09-04 17:52:39 +0300

    Added a fix to deprecated query still being used

--- src/routes/projects_.$projectId.tracking.tsx
+++ src/routes/projects_.$projectId.tracking.tsx
@@ -7,8 +7,8 @@ import {
   useListChangeProposalsQuery,
   useListJobPositionsQuery,
   useListNotificationEventsQuery,
-  useListProjectUsersQuery,
   useListTasksQuery,
+  useListUsersQuery,
 } from "hooks/api-queries";
 import TaskList from "components/tasks/task-list";
 import { useAtom } from "jotai";
@@ -33,8 +33,8 @@ function TrackingIndexRoute() {
   const findUserQuery = useFindUserQuery(auth?.token.userId ?? "");
   const user = findUserQuery.data;
 
-  const listUsersQuery = useListProjectUsersQuery(projectId);
-  const users = listUsersQuery.data || [];
+  const listUsersQuery = useListUsersQuery({ projectId });
+  const users = listUsersQuery.data?.users || [];
 
   const listTasksQuery = useListTasksQuery({ projectId: projectId });
   const tasks = listTasksQuery.data || [];
